bbff23c5224d9e373a2c6664de6b731f5c1c3900
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2021-05-12 12:00:20 +0500

    Just Started

---
+++ README.md
@@ -0,0 +1,6 @@
+# catalogbypk
+
+A new Flutter project demo Pawan Kumar using Velocity x.
+
+## Getting Started
+
---
+++ android/app/src/main/kotlin/com/example/catalogbypk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.catalogbypk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
